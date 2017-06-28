9d633bb7712b891ac82bc8f08f80a53aec2a91af
tuan <vanhtuan0409@gmail.com>
2017-06-28 18:59:22 +0900

    update babel config and add env variable

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log("aaaaa");
+function resolveAfter2Seconds(x) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(x);
+    }, 2000);
+  });
+}
+
+async function add1(x) {
+  var a = resolveAfter2Seconds(20);
+  var b = resolveAfter2Seconds(30);
+  return x + (await a) + (await b);
+}
+
+add1(10).then(v => console.log(v));
+console.log(process.env["HELLO"]);
