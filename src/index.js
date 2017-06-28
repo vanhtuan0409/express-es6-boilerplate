b2508c9860eb3b960d18ca0e7e635e3ac35fac3e
tuan <vanhtuan0409@gmail.com>
2017-06-28 19:10:48 +0900

    add middlewares

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
-function resolveAfter2Seconds(x) {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      resolve(x);
-    }, 2000);
-  });
-}
+import express from "express";
+import morgan from "morgan";
+import bodyParser from "body-parser";
+import cors from "cors";
 
-async function add1(x) {
-  var a = resolveAfter2Seconds(20);
-  var b = resolveAfter2Seconds(30);
-  return x + (await a) + (await b);
-}
+const app = express();
+app.use(morgan("dev"));
+app.use(cors());
+app.use(bodyParser.json());
 
-add1(10).then(v => console.log(v));
-console.log(process.env["HELLO"]);
+app.get("/", function(req, res) {
+  res.send("Hello world");
+});
+
+const port = process.env.HTTP_PORT || 3000;
+app.listen(port, function() {
+  console.log(`Server running on :${port}`);
+});
